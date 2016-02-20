58b7d87c2ec5646afc4eb8e574b75eca798a513c
Brooke Kuhlmann <brooke@alchemists.io>
2016-02-20 10:23:58 -0700

    Updated main script format and documentation.

--- settings/main.sh
+++ settings/main.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 # DESCRIPTION
 # Defines global settings.
@@ -7,5 +7,5 @@
 # General
 set -o nounset # Exit, with error message, when attempting to use an undefined variable.
 set -o errexit # Abort script at first error, when a command exits with non-zero status.
-set -o pipefail # Returns exit status of the last command in the pipe that returned a non-zero return value.
-IFS=$'\n\t' # Defines how Bash splits words and iterates arrays. This defines newlines and tabs as delimiters.
+set -o pipefail # Return exit status of the last command in the pipe that returned a non-zero return value.
+IFS=$'\n\t' # Defines newlines and tabs as delimiters for splitting words and iterating arrays.
